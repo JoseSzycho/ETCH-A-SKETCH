25f94b492c0fccb8d71e251d62a65866e4092de4
Jose Szychowski <joseszychowski@gmail.com>
2022-07-12 20:20:33 -0300

    Modify createSketch so always keep same size, no matter grid size

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function boxPaint(){
-    this.setAttribute('style', 'background-color:black'); //sets the box to black
+    this.style.backgroundColor = 'black';
 }
 
 function createSketch(gridSize = 16){
@@ -17,6 +17,8 @@ function createSketch(gridSize = 16){
             //creating and adding divs
             var div = document.createElement('div');
             div.classList.add('single-box');
+            div.style.height = `${Math.round(960/gridSize)}px`; //keeps 960px as max sketch height
+            div.style.width = `${Math.round(960/gridSize)}px`;  //keeps 960px as max width
             //div.textContent = (i); //adds number to box for understanding the positions
             div.addEventListener('mouseover',boxPaint);
             divColumn.appendChild(div);
