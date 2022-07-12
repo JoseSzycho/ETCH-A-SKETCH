59a31234a4eeda1533ea91065ba6dfcab5b67408
Jose Szychowski <joseszychowski@gmail.com>
2022-07-12 20:01:47 -0300

    Add gridSizeChange function

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ function boxPaint(){
     this.setAttribute('style', 'background-color:black'); //sets the box to black
 }
 
-function createSketch(){
+function createSketch(gridSize = 16){
 
     const container = document.querySelector('#sketch-container');
+    container.innerHTML = (''); //cleans any previous created grid, so it can create a new one when gridSizeChange invoked
     
-    for (let i = 0; i < 16; i++){ //creates "x" number of columns
-        for(let i = 0; i < 16; i++ ){
+    for (let i = 0; i < gridSize; i++){ //creates "x" number of columns
+        for(let i = 0; i < gridSize; i++ ){
             if(i==0){ //create a single column where the "x" divs will be added
                 var divColumn = document.createElement('div');
                 divColumn.classList.add('column');
@@ -24,5 +25,14 @@ function createSketch(){
 
     
 };
+
+function gridSizeChange(){
+    const gridSize = prompt('Enter new grid size:');
+    createSketch(gridSize);
+}
+
+const gridSize = document.querySelector('#grid-size');
+gridSize.addEventListener('click', gridSizeChange);
+
 
 createSketch();
