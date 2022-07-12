448dac48089d1480074ef5df6db3709ae09f4f99
Jose Szychowski <joseszychowski@gmail.com>
2022-07-12 19:40:46 -0300

    Create functional etch-sket

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+function boxPaint(){
+    this.setAttribute('style', 'background-color:black'); //sets the box to black
+}
+
 function createSketch(){
 
     const container = document.querySelector('#sketch-container');
     
-    for (let i = 0; i < 16; i++){
+    for (let i = 0; i < 16; i++){ //creates "x" number of columns
         for(let i = 0; i < 16; i++ ){
-            if(i==0){ //creates column where the the div will be added
+            if(i==0){ //create a single column where the "x" divs will be added
                 var divColumn = document.createElement('div');
                 divColumn.classList.add('column');
                 container.appendChild(divColumn);
@@ -12,8 +16,9 @@ function createSketch(){
             //creating and adding divs
             var div = document.createElement('div');
             div.classList.add('single-box');
-            div.textContent = (i);
-            divColumn.appendChild(div)
+            //div.textContent = (i); //adds number to box for understanding the positions
+            div.addEventListener('mouseover',boxPaint);
+            divColumn.appendChild(div);
         };
     };
 
